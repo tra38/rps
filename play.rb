5e8887805f929b036aa5f89a6bb2fdb991595ba8
tra38 <tra38@users.noreply.github.com>
2015-03-09 13:50:09 -0500

    Made comments readable by splitting them up.

--- play.rb
+++ play.rb
@@ -1,5 +1,6 @@
 def play
-@VictoryCheck = {"paper" => "rock", "rock" => "scissors", "scissors" => "paper"} #Key = Move, Value = Move That the Key Can Defeat.
+@VictoryCheck = {"paper" => "rock", "rock" => "scissors", "scissors" => "paper"}
+#Key = Move, Value = Move That the Key Can Defeat.
 puts "Let's Play Rock, Paper, Scissors! To play, pick one of those three options."
 player = gets.chomp.downcase.to_s
 if player == ("lizard") || player == ("spock")
@@ -13,7 +14,9 @@ puts "The player has chosen #{player}."
 puts "The computer responds with #{computer}!"
 if player == computer
 	puts "It's a tie!"
-elsif player == @VictoryCheck[computer] #We look up the Computer's move in the VictoryCheck Hash, and find its value. We then check to see if this value matches the Player's move. If it does, then the Computer defeats the Player.
+elsif player == @VictoryCheck[computer] #We look up the Computer's move in the VictoryCheck Hash,
+#and find its value. We then check to see if this value matches the Player's move. If it does, 
+#then the Computer defeats the Player.
 	puts "Computer wins!"
 else
 	puts "Player wins!"
