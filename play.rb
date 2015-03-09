e5e8bfe6bd99df365150738e4a9f39ee3f4ee325
tra38 <tra38@users.noreply.github.com>
2015-03-09 13:39:44 -0500

    Refactored the code.
    Previously, I used 'case statements' to determine if the Computer wins or loses, but searching up other Ruby solutions revealed that many people preferred storing all the possible victory conditions via a Hash. I debated whether it would actually prove useful, but ultimately decided to give into the consensus simply because it makes it easier to expand RPS to include variants.
    
    Credit for this change mostly goes to https://github.com/aterreno/rock-paper-scissors/blob/master/lib/game_engine.rb
    Aterreno's Ruby program is a bit more complicated than mine though, needing multiple files to run properly, and the need to specify the moves of both players before the program can determine the winner.

--- play.rb
+++ play.rb
@@ -1,10 +1,11 @@
 def play
+@VictoryCheck = {"paper" => "rock", "rock" => "scissors", "scissors" => "paper"} #Key = Move, Value = Move That the Key Can Defeat.
 puts "Let's Play Rock, Paper, Scissors! To play, pick one of those three options."
 player = gets.chomp.downcase.to_s
 if player == ("lizard") || player == ("spock")
 	raise ArgumentError.new("The computer thinks you're a nerd and refuses to play.")
 end
-if player != ("rock") && player != ("paper") && player != ("scissors")
+if player != ("rock") && player != ("paper") && player != ("scissors") #This can probably be refactored away.
 	raise ArgumentError.new("The only input allowed is Rock, Paper, or Scissors.")
 end
 computer = ["Rock","Paper","Scissors"].sample.chomp.downcase
@@ -12,24 +13,9 @@ puts "The player has chosen #{player}."
 puts "The computer responds with #{computer}!"
 if player == computer
 	puts "It's a tie!"
-	return
+elsif player == @VictoryCheck[computer] #We look up the Computer's move in the VictoryCheck Hash, and find its value. We then check to see if this value matches the Player's move. If it does, then the Computer defeats the Player.
+	puts "Computer wins!"
 else
-  case player
-    when "rock"
-      if computer == "paper"
-        puts "Computer wins!"
-      else puts "Player win!"
-    end
-    when "paper"
-      if computer == "scissors"
-        puts "Computer wins!"
-      else "Player wins!"
-    end
-  when "scissors"
-    if computer == "rock"
-      puts "Computer wins!"
-    else "Player wins!"
-    end
-  end
+	puts "Player wins!"
 end
 end
