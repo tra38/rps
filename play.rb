21f8ae7f11010938796e25a8e3593c8f1aadb2a9
tra38 <tra38@users.noreply.github.com>
2015-03-09 11:45:38 -0500

    Added error messages in case of non-expected input

--- play.rb
+++ play.rb
@@ -1,6 +1,12 @@
 def play
-puts "Let's Play Rock, Paper, Scissors!"
+puts "Let's Play Rock, Paper, Scissors! To play, pick one of those three options."
 player = gets.chomp.downcase.to_s
+if player == ("lizard") || player == ("spock")
+	raise ArgumentError.new("The computer thinks you're a nerd and refuses to play.")
+end
+if player != ("rock") && player != ("paper") && player != ("scissors")
+	raise ArgumentError.new("The only input allowed is Rock, Paper, or Scissors.")
+end
 computer = ["Rock","Paper","Scissors"].sample.chomp.downcase
 puts "The player has chosen #{player}."
 puts "The computer responds with #{computer}!"
